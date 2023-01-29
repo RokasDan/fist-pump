950a180df24c8be8ac18827aa96c077819311e2a
RokasDan <rokas108@gmail.com>
2023-01-29 19:38:07 +0000

    Added vector rotation relative to the camera forward vector

--- Assets/Scripts/Runtime/PlayerController.cs
+++ Assets/Scripts/Runtime/PlayerController.cs
@@ -12,6 +12,9 @@ namespace RokasDan.FistPump.Runtime
         [SerializeField]
         private Rigidbody rigidBody;
 
+        [SerializeField]
+        private Transform cameraTransform;
+
         [Header("Forces")]
         [Min(0f)]
         [SerializeField]
@@ -28,13 +31,24 @@ namespace RokasDan.FistPump.Runtime
         [SerializeField]
         private InputActionReference moveInputAction;
 
+        [SerializeField]
+        private InputActionReference lookInputAction;
 
+        [Header("Gizmos")]
+        [SerializeField]
+        private float gizmoLenght = 3f;
+
+        private Vector3 flattenedLookDirection;
+        private Quaternion flattenedLookRotation;
         private Vector3 absoluteMoveDirection;
+        private Vector3 relativeMoveDirection;
 
         private void OnEnable()
         {
             //Subscribing the methods to prevent memory leaks
             jumpInputAction.action.performed += OnJumpPerformed;
+
+            lookInputAction.action.performed += OnLookPreformed;
 
             moveInputAction.action.performed += OnMovePerformed;
             moveInputAction.action.canceled += OnMoveCanceled;
@@ -44,6 +58,8 @@ namespace RokasDan.FistPump.Runtime
         {
             //Unsubscribing the methods to prevent memory leaks
             jumpInputAction.action.performed -= OnJumpPerformed;
+
+            lookInputAction.action.performed -= OnLookPreformed;
 
             moveInputAction.action.performed -= OnMovePerformed;
             moveInputAction.action.canceled -= OnMoveCanceled;
@@ -58,6 +74,21 @@ namespace RokasDan.FistPump.Runtime
             {
                 Move();
             }
+        }
+
+        private void OnLookPreformed(InputAction.CallbackContext context)
+        {
+            Debug.Log("Looking Around", this);
+
+            //Getting camera forward transform.
+            var cameraDirection = cameraTransform.forward;
+
+            //Flattening the camera forward transform to the Y plane.
+            flattenedLookDirection = Vector3.ProjectOnPlane(cameraDirection, Vector3.up);
+
+            // Getting the angle which will rotate the absolute vectors from WASD keys.
+            flattenedLookRotation = Quaternion.LookRotation(flattenedLookDirection);
+
         }
 
         // If WASD or arrows are pressed we start apply a Vector3 to our move direction.
@@ -86,14 +117,39 @@ namespace RokasDan.FistPump.Runtime
         // Our move function which adds velocity to our 3D vector.
         private void Move()
         {
-            rigidBody.AddForce(absoluteMoveDirection * moveForce);
+            // Applying the rotation of the camera forward vector to the WASD absolute vector.
+            relativeMoveDirection = flattenedLookRotation * absoluteMoveDirection;
+
+            // Normalizing and adding velocity to the rotated absolute vector.
+            rigidBody.AddForce(relativeMoveDirection.normalized * moveForce);
         }
+
         // Our Jump function adds velocity to Y vector.
         private void Jump()
         {
             var currentVelocity = rigidBody.velocity;
             currentVelocity.y += jumpSpeed;
             rigidBody.velocity = currentVelocity;
+        }
+
+
+        //Gizmos
+
+        private void OnDrawGizmos()
+        {
+            //Camera look directly from camera transform.
+            Gizmos.color = Color.green;
+            Gizmos.DrawRay(transform.position + new Vector3(0, 0.45f, 0), cameraTransform.forward.normalized * gizmoLenght
+            );
+
+            //Camera look flattened to the Y coordinate.
+            Gizmos.color = Color.blue;
+            Gizmos.DrawRay(transform.position, flattenedLookDirection.normalized * gizmoLenght
+            );
+
+            // Relative Move direction of the WASD absolute vectors when the rotation of the camera forward vector is applied.
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(transform.position, relativeMoveDirection.normalized * gizmoLenght);
         }
     }
 }
